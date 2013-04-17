cd9037b9b14c0a9940494100d786a893e0875bd5
Drarok Ithaqua <source@catchall.drarok.com>
2013-04-17 16:14:03 +0100

    Add support for a 'default' value for elements.

--- classes/Konform/Konform.php
+++ classes/Konform/Konform.php
@@ -166,7 +166,10 @@ abstract class Konform_Konform
 			$element->name = $fieldName;
 			$element->label = Arr::get($fieldOptions, 'label', $fieldName);
 			$element->required = Arr::get($fieldOptions, 'required', false);
-			$element->value = Arr::get($this->_data, $fieldName, '');
+			if (($value = Arr::get($this->_data, $fieldName, FALSE)) === FALSE) {
+				$value = Arr::get($fieldOptions, 'default', '');
+			}
+			$element->value = $value;
 			$class = Arr::get($fieldOptions, 'class', '');
 			if (array_key_exists($fieldName, $this->_errors)) {
 				$class .= ' error';
